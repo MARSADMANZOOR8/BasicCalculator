51eb9ec2c87e66a9898f5e421e75ab679513b7c3
MARSAD MANZOOR <120159246+MARSADMANZOOR8@users.noreply.github.com>
2023-12-24 22:48:25 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the First Number\n "))
+b = int(input("Enter the Second Number\n "))
+c = input("Enter the operation\n")
+if(c == "+"):
+    d = a + b
+elif(c == "-"):
+    d = a - b
+elif(c == "*"):
+    d = a * b
+elif(c == "/"):
+    d = a / b
+print ("Result !!!!!!!\n "  )
+print (d)
